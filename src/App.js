cd04cfffe4f51581b2a88ed4166728a06c7326e3
Ashraf Ali <ali4282271@gmail.com>
2024-04-21 01:02:22 -0700

    updated 21/4/24

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Deals from "./Component/Deals/Deals";
 import Home from "./Component/Home/Home";
 import NewArivals from "./Component/New Arivals/NewArivals";
+import Packages from "./Component/Packages/Packages";
+import Memo from "./Component/memo/memo";
+import SignIn from "./Component/SignIn/SignIn";
 
 
 
@@ -16,6 +19,10 @@ function App() {
           <Route path="Deals" element={<Deals />} />
           <Route path="NewArivals" element={<NewArivals />} />
           <Route path="contact" element={<NewArivals />} />
+          <Route path="Packages" element={<Packages />} />
+          <Route path="Memo" element={<Memo />} />
+          <Route path="SignIn" element={<SignIn /> } />
+
 
           {/* <Route path="*" element={<NoPage />} /> */}
       </Routes>
